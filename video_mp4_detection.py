32d0c9d53969abc41485988abd37a03ce5dda98d
ismailalptug <alptugismaill@gmail.com>
2021-08-02 14:54:59 +0300

    Update video_mp4_detection.py

--- video_mp4_detection.py
+++ video_mp4_detection.py
@@ -13,19 +13,10 @@ layer_names = net.getLayerNames()
 output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 
 
-#font = cv2.FONT_HERSHEY_PLAIN
-#starting_time = time.time()
-
-
-# frame_id = 0
 class DisplayTask(object):
-    """
-    """
+
     def __init__(self, name="frame",show=True):
-        """
-        :param width:
-        :param height:
-        """
+
         self.win_name = name
         self.win_show = show
 
@@ -49,7 +40,7 @@ def readPlate(currentFrame,dt):
 
     frame= currentFrame["img"][::1]
 
-    # frame_id += 1
+
     height, width, channels = frame.shape
 
     blob = cv2.dnn.blobFromImage(frame, 0.00392, (416, 416), (0, 0, 0), True, crop=False)
@@ -65,14 +56,12 @@ def readPlate(currentFrame,dt):
             class_id = np.argmax(scores)
             confidence = scores[class_id]
             if confidence > 0.3:
-                # Object detected
-                # print(class_id)
+
                 center_x = int(detection[0] * width)
                 center_y = int(detection[1] * height)
                 w = int(detection[2] * width)
                 h = int(detection[3] * height)
 
-                # Rectangle coordinates
                 x = int(center_x - w / 2)
                 y = int(center_y - h / 2)
 
@@ -81,7 +70,7 @@ def readPlate(currentFrame,dt):
                 class_ids.append(class_id)
 
     indexes = cv2.dnn.NMSBoxes(boxes, confidences, 0.5, 0.4)
-    #font = cv2.FONT_HERSHEY_PLAIN
+
 
     for i in range(len(boxes)):
         if i in indexes:
@@ -89,30 +78,27 @@ def readPlate(currentFrame,dt):
             label = str(classes[class_ids[i]])
             color = (0, 255, 0)
             cv2.rectangle(frame, (x, y), (x + w, y + h), color, 1)
-            # cv2.putText(img, label, (x + int(w * 0.9) + 20, y + int(h/2) + 10), font, 1.5, color, 2)
+
 
             mask = np.zeros(frame.shape[0:2], dtype="uint8")
             cv2.rectangle(mask, (x + int(w * 0.13), y), (x + w - int(w * 0.08), y + h), 255, -1)
-            #cv2.imshow("Rectangular Mask" ,mask)
-            #masked = cv2.bitwise_and(frame, frame, mask=mask)
-            # cv2.imshow("Mask Applied to Image", masked)
+
             (x, y) = np.where(mask == 255)
             (x1, y1) = (np.min(x), np.min(y))
             (x2, y2) = (np.max(x), np.max(y))
             cropped_image = frame[x1:x2, y1:y2]
             cropped_image = cv2.resize(cropped_image, None, fx=1 / 0.2, fy=1 / 0.2)
-            #plate = cv2.bitwise_not(cropped_image)
+
             plate = cv2.cvtColor(cropped_image, cv2.COLOR_BGR2GRAY)
             plate = cv2.adaptiveThreshold(plate, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 101, 11)
             kernel_size = 3
             kernel = np.ones((kernel_size, kernel_size), np.uint8)
             plate = cv2.erode(plate, kernel, iterations=1)
-            # plate = cv2.morphologyEx(plate, cv2.MORPH_CLOSE, kernel)
+
             plate = cv2.medianBlur(plate, kernel_size)
-            # cv2.imshow("Plaka", plate)
+
             dt.show( plate,"Plaka1" )
-            # print(pytesseract.image_to_string(plate, lang='eng',
-            # config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8 --oem 3'))
+
             text = pytesseract.image_to_string(plate, lang='eng',
                                                config='-c tessedit_char_whitelist=0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ --psm 8 --oem 3')
 
@@ -120,15 +106,6 @@ def readPlate(currentFrame,dt):
             text = text.split()
             print(text)
 
-    #for i in range(len(boxes)):
-    #    if i in indexes:
-    #        x, y, w, h = boxes[i]
-    #        color = (0, 255, 0)
-    #        cv2.rectangle(frame, (x, y), (x + w, y + h), color, 1)
-    #        if (text == []):
-    #            cv2.putText(frame, label, (x + int(w * 0.9) + 30, y + int(h / 2) + 10), font, 1.5, color, 2)
-    #        else:
-    #            cv2.putText(frame, text[0], (x + int(w * 0.9) + 30, y + int(h / 2) + 10), font, 1.5, color, 2)
 
   except Exception as e:
       print(e)
@@ -137,7 +114,7 @@ def readPlate(currentFrame,dt):
 
 
 def capture(currentFrame,dt):
-    #cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
     cap = cv2.VideoCapture("./1.mp4")
     while True:
         _,  frame = cap.read()
